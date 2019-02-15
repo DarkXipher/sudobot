d0979075dc78e8ea3f9406ec2b68eb6baa7567ef
DarkXipher <darx78@yahoo.com>
2019-02-15 10:16:23 -0600

    test connection works, converting sqlite to pg

---
+++ node-backend/src/commands/random/query.js
@@ -0,0 +1,49 @@
+const { Command } = require('discord.js-commando');
+const { Database } = require('../../database.js');
+const { Pool } = require('pg');
+
+const connString = process.env.DATABASE_URL || 'postgres://localhost:5432/sudobot';
+
+
+module.exports = class queryCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'query',
+			group: 'random',
+			memberName: 'query',
+            description: 'Helper function to test the DB out',
+            clientPermissions: ['ADMINISTRATOR'],
+            userPermissions: ['ADMINISTRATOR'],
+            args: [
+				{
+					'key': 'sql',
+					'prompt': 'query to run',
+					'type': 'string'
+				}]
+        });
+        
+        this.db = new Pool({
+            connectionString: connString
+        });
+	}
+
+	run(message, args) {
+        console.log(connString);
+        if(!args.sql) {
+            return message.reply('Please enter a valid query.');
+        }
+        console.log("Query being ran with args: " + args.sql);
+
+
+
+		let result = this.db.query(args.sql)
+		.then( res => {
+			console.log(res);
+
+			res.rows.forEach(function(row){
+				console.log(row);
+				message.say(row);
+			});
+		});
+	}
+};
---
+++ node-backend/src/commands/random/testdb.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord.js-commando');
+const { Database } = require('../../database.js');
+const { Pool } = require('pg');
+
+const connString = process.env.DATABASE_URL || 'postgres://localhost:5432/sudobot';
+
+
+module.exports = class testDBCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'testdb',
+			group: 'random',
+			memberName: 'testdb',
+			description: 'Helper function to test the DB out',
+        });
+        
+        this.db = new Pool({
+            connectionString: connString
+        });
+	}
+
+	run(message) {
+		console.log(connString);
+
+		let result = this.db.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';")
+		.then( res => {
+			console.log(res);
+
+			res.rows.forEach(function(row){
+				console.log("Table name: " + row.table_name);
+				message.say("Table name: " + row.table_name);
+			});
+		});
+	}
+};
