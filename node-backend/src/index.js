606ee85c247115c8821019917b6548278c84013e
DarkXipher <darx78@yahoo.com>
2019-02-18 15:34:47 -0600

    fixing bugs to swap to class based client/db

--- node-backend/src/index.js
+++ node-backend/src/index.js
@@ -40,17 +40,16 @@ const botPrefix = process.env.BOT_PREFIX || '>sudo';
     let bot = null;
     let start = function () {
         let webDB = new Database(1000);
-        webDB.init().then((w) => {
-            setTimeout(() => {
-                webDB.loadSettings('bot').then((bSettings) => {
-                    if (bSettings && bSettings.token) {
-                        bot = new BotClient(webDB, bSettings.token, bSettings.ownerid, bSettings.commandprefix, bSettings.unknowncommandresponse);
-                        bot.init().catch(() => { console.error('Failed initializing DiscordBot. Is your token correct?') });
-                    } else console.log('There is no bot token provided. Please check your configuration!');
-                    // new WebClient(webDB, bot).init();
-                });
-            }, 1000);
-        });
+        webDB.init();
+        setTimeout(() => {
+            webDB.loadSettings('bot').then((bSettings) => {
+                if (bSettings && bSettings.token) {
+                    bot = new BotClient(webDB, bSettings.token, bSettings.ownerid, bSettings.commandprefix, bSettings.unknowncommandresponse);
+                    bot.init().catch(() => { console.error('Failed initializing DiscordBot. Is your token correct?') });
+                } else console.log('There is no bot token provided. Please check your configuration!');
+                // new WebClient(webDB, bot).init();
+            });
+        }, 1000);
     }
     
     start();
