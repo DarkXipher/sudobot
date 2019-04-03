62ffeb749e7c7a88160dee0c6790e2f479827163
DarkXipher <darx78@yahoo.com>
2019-04-03 17:02:47 -0500

    Fixed Choose command

--- node-backend/src/commands/random/choose.js
+++ node-backend/src/commands/random/choose.js
@@ -6,25 +6,23 @@ module.exports = class chooseCommand extends Command {
 			name: 'choose',
 			group: 'random',
 			memberName: 'choose',
-            description: 'The one stop picker for hard choices!',
-            clientPermissions: ['ADMINISTRATOR'],
-            userPermissions: ['ADMINISTRATOR'],
-            args: [
+			description: 'The one stop picker for hard choices!',
+			clientPermissions: ['ADMINISTRATOR'],
+			userPermissions: ['ADMINISTRATOR'],
+			args: [
 				{
 					'key': 'choice',
 					'prompt': 'query to run',
 					'type': 'string'
 				}]
-        });
+		});
 	}
 
 	run(message, args) {
-		console.log(args.choice);
 		var arr = args.choice.split(/ +/);
-		console.log(arr);
-        var results = Math.ceil(Math.random() * arr.length);
-        results = arr[(results - 1)];
-    
-        message.channel.send(`${message.author.username}, I think **${results}** would be the best choice!`);
+		var results = Math.ceil(Math.random() * arr.length);
+		results = arr[(results - 1)];
+
+		message.channel.send(`${message.author.username}, I think **${results}** would be the best choice!`);
 	}
 };
