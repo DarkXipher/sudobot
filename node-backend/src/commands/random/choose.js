23a4e56f366c6ded8b9f58a83d7127759ace8b46
DarkXipher <darx78@yahoo.com>
2019-04-03 16:54:44 -0500

    removing commands from structure to allow testing

--- node-backend/src/commands/random/choose.js
+++ node-backend/src/commands/random/choose.js
@@ -18,9 +18,9 @@ module.exports = class chooseCommand extends Command {
         });
 	}
 
-	run(message, choice) {
-		console.log(choice);
-		var arr = choice.split(/  +/);
+	run(message, args) {
+		console.log(args.choice);
+		var arr = args.choice.split(/  +/);
         var results = Math.ceil(Math.random() * arr.length);
         results = arr[(results - 1)];
     
